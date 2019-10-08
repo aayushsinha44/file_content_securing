d6a2aa44550aeaa932c1e539f4382cd51cd1d5f4
Aayush <aayush@localhost.localdomain>
2019-10-08 19:38:31 +0530

    file name update added

--- blockchain/src/blockchain/blockchain.go
+++ blockchain/src/blockchain/blockchain.go
@@ -15,8 +15,12 @@ import (
 )
 
 type BlockChain struct {
-	Client *ethclient.Client
-	Auth   *bind.TransactOpts
+	Client    *ethclient.Client
+	Auth      *bind.TransactOpts
+	AuthQuery *bind.CallOpts
+
+	PrivateKey  *ecdsa.PrivateKey
+	FromAddress common.Address
 }
 
 type BlockChainInfo struct {
@@ -48,6 +52,30 @@ func GetUserInfo(privateKey string) (*UserInfo, error) {
 	return userInfo, nil
 }
 
+func (bc *BlockChain) getAuthTransactOpts(ctx context.Context) (*bind.TransactOpts, error) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	nonce, err := bc.Client.PendingNonceAt(ctx, bc.FromAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	gasPrice, err := bc.Client.SuggestGasPrice(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	gasLimit := uint64(300000 * 10)
+	auth := bind.NewKeyedTransactor(bc.PrivateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = gasLimit
+	auth.GasPrice = gasPrice
+	return auth, nil
+}
+
+// MakeConnection : Makes connection with the blockchain
 func (ui *UserInfo) MakeConnection(ctx context.Context, info *BlockChainInfo) (*BlockChain, error) {
 	if ctx == nil {
 		ctx = context.Background()
@@ -55,15 +83,6 @@ func (ui *UserInfo) MakeConnection(ctx context.Context, info *BlockChainInfo) (*
 	if info == nil {
 		return nil, fmt.Errorf("Blockchain information is required")
 	}
-	// if info.ClientAddresss == nil {
-	// 	return nil, fmt.Errorf("Blockchain client address is required")
-	// }
-	// if info.IsHttps == nil {
-	// 	// info.IsHttps = false
-	// }
-	// if info.ClientPort == nil {
-	// 	// info.ClientPort = 80
-	// }
 	var address string
 	if info.IsHttps {
 		address = fmt.Sprintf("%s%s:%d", "https://", info.ClientAddress, info.ClientPort)
@@ -93,12 +112,16 @@ func (ui *UserInfo) MakeConnection(ctx context.Context, info *BlockChainInfo) (*
 	auth.GasLimit = gasLimit
 	auth.GasPrice = gasPrice
 
-	blockchain := &BlockChain{Client: client, Auth: auth}
+	authQuery := &bind.CallOpts{From: ui.FromAddress}
+
+	blockchain := &BlockChain{Client: client, Auth: auth,
+		AuthQuery: authQuery, PrivateKey: ui.PrivateKey, FromAddress: ui.FromAddress}
 
 	return blockchain, nil
 
 }
 
+// StoreFile : Store file in the blockchain
 func (b *BlockChain) StoreFile(fileName string) (string, error) {
 
 	address, _, _, err := file.DeployFile(b.Auth, b.Client, fileName)
@@ -106,4 +129,55 @@ func (b *BlockChain) StoreFile(fileName string) (string, error) {
 		return "", err
 	}
 	return address.Hex(), nil
+}
+
+type FileAddress struct {
+	Address string
+}
+
+func (fa *FileAddress) GetHexAddress() common.Address {
+	return common.HexToAddress(fa.Address)
+}
+
+func (b *BlockChain) GetFileName(fa *FileAddress) (string, error) {
+	instance, err := file.NewFile(fa.GetHexAddress(), b.Client)
+	if err != nil {
+		return "", err
+	}
+	fileName, err := instance.FileName(b.AuthQuery)
+	if err != nil {
+		return "", err
+	}
+	return fileName, nil
+}
+
+func (b *BlockChain) UpdateFileName(fa *FileAddress, fileName string) (string, error) {
+	instance, err := file.NewFile(fa.GetHexAddress(), b.Client)
+	if err != nil {
+		return "", err
+	}
+
+	auth, err := b.getAuthTransactOpts(nil)
+	if err != nil {
+		return "", err
+	}
+
+	_, err = instance.UpdateFileName(auth, fileName)
+	if err != nil {
+		return "failed", err
+	}
+	return "success", err
+}
+
+func (b *BlockChain) GetOwnerList(fa *FileAddress) ([]common.Address, error) {
+	instance, err := file.NewFile(fa.GetHexAddress(), b.Client)
+	if err != nil {
+		return nil, err
+	}
+	// TODO changes bytes to hex
+	ownerList, err := instance.GetOwnerList(b.AuthQuery)
+	if err != nil {
+		return nil, err
+	}
+	return ownerList, nil
 }
--- blockchain/src/main.go
+++ blockchain/src/main.go
@@ -20,9 +20,35 @@ func main() {
 
 	bc, err := userInfo.MakeConnection(nil, blockChainInfo)
 	must(err)
+
+	// Creating new file
 	address, err := bc.StoreFile("TestFile")
 	must(err)
-	fmt.Println(address)
+	fmt.Println("Address of File:", address)
+
+	// Getting file name
+	fa := &blockchain.FileAddress{Address: address}
+	fileName, err := bc.GetFileName(fa)
+	must(err)
+	fmt.Println("File Name:", fileName)
+
+	// Updating file Name
+	status, err := bc.UpdateFileName(fa, "TestFileChanged")
+	must(err)
+	fmt.Println("File Name Changing: ", status)
+
+	fileName, err = bc.GetFileName(fa)
+	must(err)
+	fmt.Println("New File Name:", fileName)
+
+	// Getting owner list
+	addressList, err := bc.GetOwnerList(fa)
+	must(err)
+	fmt.Println(addressList)
+	// for index, element := range addressList {
+	// 	fmt.Println(index, element)
+	// }
+
 }
 
 func must(err error) {
